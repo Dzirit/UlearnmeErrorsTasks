1d0d484985a161c34b5b476dd69176e55e22891f
Dzirit <41745559+Dzirit@users.noreply.github.com>
2020-05-26 00:17:48 +0300

    renames solution and projects. Add project Branching

---
+++ Branchings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Branchings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
+            Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
+            Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
+            Console.WriteLine(MiddleOf(2, 3, 2));
+            Console.WriteLine(MiddleOf(8, 8, 8));
+            Console.WriteLine(MiddleOf(5, 0, 1));
+        }
+        public static int MiddleOf(int a, int b, int c)
+        {
+            if (a >= b && a <= c || a <= b && a >= c) return a;
+            else if (b >= a && b <= c || b <= a && b >= c) return b;
+            else return c;
+        }
+    }
+}
