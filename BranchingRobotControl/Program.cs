21c1282d58747b9a924ccf3a2a5787cbd4bd638c
Dzirit <41745559+Dzirit@users.noreply.github.com>
2020-05-26 00:44:06 +0300

    refactor method ShouldFire

---
+++ BranchingRobotControl/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BranchingRobotControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Fire?:{ShouldFire(true,"boss1",12)}");
+        }
+		static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+		{
+			//bool shouldFire = true;
+			//if (enemyInFront == true)
+			//{
+			//	if (enemyName == "boss")
+			//	{
+			//		if (robotHealth < 50) shouldFire = false;
+			//		if (robotHealth > 100) shouldFire = true;
+			//	}
+			//}
+			//else
+			//{
+			//	return false;
+			//}
+			//return shouldFire;
+			return enemyInFront && !(enemyName=="boss" && robotHealth<50);
+		}
+	}
+}
